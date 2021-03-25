c7ca5ea0da92bd5abc85ede847ef16aa6e5937e1
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-24 23:09:32 -0700

    ADD: Most code moved out of main. Updated to draw welcome message and tildas to screen and to allow cursor movement

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+mod terminal;
 
-fn die(e: std::io::Error) {
-    panic!(e);
-}
+use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Position;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    let b = c as u8;
-                    if c.is_control() {
-                        println!("{:03?} ({:#010b})\r", b, b);
-                    } else {
-                        println!("{:03?} ({:#010b}) ({})\r", b, b, c);
-                    }
-                },
-                Key::Ctrl('q') => break,
-                _ => println!("{:03?}\r", key),
-            }
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run();
 }
