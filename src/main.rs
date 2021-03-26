430500b768891701922f9b712480ea7414b55ec6
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-25 21:51:16 -0700

    ADD: File loading, robust cursor movement, status and message bars

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
+mod document;
+mod row;
 mod terminal;
 
 use editor::Editor;
 pub use terminal::Terminal;
+pub use document::Document;
+pub use row::Row;
 pub use editor::Position;
 
 fn main() {
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,5 +1,8 @@
 use crate::Position;
+
 use std::io::{self, stdout, Write};
+
+use termion::color;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
@@ -21,7 +24,7 @@ impl Terminal {
         Ok(Self {
             size: Size {
                 width: size.0,
-                height: size.1,
+                height: size.1.saturating_sub(2),
             },
             _stdout: stdout().into_raw_mode()?,
         })
@@ -66,5 +69,21 @@ impl Terminal {
 
     pub fn cursor_show() {
         print!("{}", termion::cursor::Show);
+    }
+
+    pub fn set_bg_color(color: color::Rgb) {
+        print!("{}", color::Bg(color));
+    }
+
+    pub fn reset_bg_color() {
+        print!("{}", color::Bg(color::Reset));
+    }
+
+    pub fn set_fg_color(color: color::Rgb) {
+        print!("{}", color::Fg(color));
+    }
+
+    pub fn reset_fg_color() {
+        print!("{}", color::Fg(color::Reset));
     }
 }
